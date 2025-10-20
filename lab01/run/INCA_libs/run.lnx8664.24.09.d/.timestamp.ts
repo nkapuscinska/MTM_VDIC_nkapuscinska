874527e679dfa307061bcc1c51e68024452d00f7
Natalia Kapuscinska <nataliakapuscinska@interia.eu>
2025-10-21 01:57:35 +0200

    done

--- lab01/run/INCA_libs/run.lnx8664.24.09.d/.timestamp.ts
+++ lab01/run/INCA_libs/run.lnx8664.24.09.d/.timestamp.ts
@@ -1,2 +1,2 @@
 1760455770 /home/student/nkapuscinska/VDIC/MTM_VDIC_nkapuscinska/common/simple_uart_switch.svp
-1761002199 /home/student/nkapuscinska/VDIC/MTM_VDIC_nkapuscinska/lab01/tb/simple_uart_switch_tb.svp
+1761004532 /home/student/nkapuscinska/VDIC/MTM_VDIC_nkapuscinska/lab01/tb/simple_uart_switch_tb.svp
