351340d08c781c00c42997f1ef411888a92d70b5
Natalia Kapuscinska <nataliakapuscinska@interia.eu>
2025-10-20 23:03:45 +0200

    refactoryzed version

---
+++ lab01/run/INCA_libs/run.lnx8664.24.09.d/.timestamp.ts
@@ -0,0 +1,2 @@
+1760455770 /home/student/nkapuscinska/VDIC/MTM_VDIC_nkapuscinska/common/simple_uart_switch.svp
+1760994079 /home/student/nkapuscinska/VDIC/MTM_VDIC_nkapuscinska/lab01/tb/simple_uart_switch_tb.svp
---
+++ lab01/run/INCA_libs/run.lnx8664.24.09.d/files.ts
@@ -0,0 +1,3 @@
+1750968485 /home/student/nkapuscinska/cds.lib
+1760455770 /home/student/nkapuscinska/VDIC/MTM_VDIC_nkapuscinska/common/simple_uart_switch.svp
+1760994079 /home/student/nkapuscinska/VDIC/MTM_VDIC_nkapuscinska/lab01/tb/simple_uart_switch_tb.svp
