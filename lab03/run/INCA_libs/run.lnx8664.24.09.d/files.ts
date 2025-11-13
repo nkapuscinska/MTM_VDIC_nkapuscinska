eca643c15a60261cd0357dbb8a57e4500c813899
Natalia Kapuscinska <nataliakapuscinska@interia.eu>
2025-11-13 01:27:10 +0100

    lab03 working wersion

--- lab03/run/INCA_libs/run.lnx8664.24.09.d/files.ts
+++ lab03/run/INCA_libs/run.lnx8664.24.09.d/files.ts
@@ -1,9 +1,9 @@
 1750968485 /home/student/nkapuscinska/cds.lib
-1762276569 /home/student/nkapuscinska/VDIC/MTM_VDIC_nkapuscinska/lab03/tb/fifomult_tb_pkg.svp
-1762278456 /home/student/nkapuscinska/VDIC/MTM_VDIC_nkapuscinska/lab03/tb/driver.svp
-1762278475 /home/student/nkapuscinska/VDIC/MTM_VDIC_nkapuscinska/lab03/tb/monitor.svp
-1762278076 /home/student/nkapuscinska/VDIC/MTM_VDIC_nkapuscinska/lab03/tb/scoreboard.svp
-1762212220 /home/student/nkapuscinska/VDIC/MTM_VDIC_nkapuscinska/lab03/tb/coverage.svp
-1762278311 /home/student/nkapuscinska/VDIC/MTM_VDIC_nkapuscinska/lab03/tb/tbgen.svp
+1762990927 /home/student/nkapuscinska/VDIC/MTM_VDIC_nkapuscinska/lab03/tb/fifomult_tb_pkg.svp
+1762993238 /home/student/nkapuscinska/VDIC/MTM_VDIC_nkapuscinska/lab03/tb/switch_bfm.svp
+1762991041 /home/student/nkapuscinska/VDIC/MTM_VDIC_nkapuscinska/lab03/tb/monitor.svp
+1762993547 /home/student/nkapuscinska/VDIC/MTM_VDIC_nkapuscinska/lab03/tb/scoreboard.svp
+1762990337 /home/student/nkapuscinska/VDIC/MTM_VDIC_nkapuscinska/lab03/tb/coverage.svp
+1762993275 /home/student/nkapuscinska/VDIC/MTM_VDIC_nkapuscinska/lab03/tb/tp_gen.svp
 1760455770 /home/student/nkapuscinska/VDIC/MTM_VDIC_nkapuscinska/common/simple_uart_switch.svp
-1762278307 /home/student/nkapuscinska/VDIC/MTM_VDIC_nkapuscinska/lab03/tb/top.svp
+1762989858 /home/student/nkapuscinska/VDIC/MTM_VDIC_nkapuscinska/lab03/tb/top.svp
--- lab03/tb/tb.f
+++ lab03/tb/tb.f
@@ -1,8 +1,8 @@
 fifomult_tb_pkg.svp
-driver.svp
+switch_bfm.svp
 monitor.svp
 scoreboard.svp
 coverage.svp
-tbgen.svp
+tp_gen.svp
 ../../common/simple_uart_switch.svp
 top.svp
