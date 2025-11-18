76cbdf881967a3c67eddee08cc672015ab17ae5f
Natalia Kapuscinska <nataliakapuscinska@interia.eu>
2025-11-18 13:55:33 +0100

    end

--- lab04/run/INCA_libs/run.lnx8664.24.09.d/files.ts
+++ lab04/run/INCA_libs/run.lnx8664.24.09.d/files.ts
@@ -1,5 +1,5 @@
 1750968485 /home/student/nkapuscinska/cds.lib
 1760455770 /home/student/nkapuscinska/VDIC/MTM_VDIC_nkapuscinska/common/simple_uart_switch.svp
-1763100524 /home/student/nkapuscinska/VDIC/MTM_VDIC_nkapuscinska/lab04/tb/fifomult_tb_pkg.svh
-1763096461 /home/student/nkapuscinska/VDIC/MTM_VDIC_nkapuscinska/lab04/tb/switch_bfm.svp
-1763100729 /home/student/nkapuscinska/VDIC/MTM_VDIC_nkapuscinska/lab04/tb/top.svp
+1763126117 /home/student/nkapuscinska/VDIC/MTM_VDIC_nkapuscinska/lab04/tb/fifomult_tb_pkg.svh
+1763126117 /home/student/nkapuscinska/VDIC/MTM_VDIC_nkapuscinska/lab04/tb/switch_bfm.svp
+1763126117 /home/student/nkapuscinska/VDIC/MTM_VDIC_nkapuscinska/lab04/tb/top.svp
---
+++ lab04_1/run/INCA_libs/run.lnx8664.24.09.d/.timestamp.ts
@@ -0,0 +1,1 @@
+1763125484 /home/student/nkapuscinska/VDIC/MTM_VDIC_nkapuscinska/lab04_1/tb/lab04_1.sv
