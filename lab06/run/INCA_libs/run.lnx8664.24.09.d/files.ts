05262a37625f912fd59b7aede274d9235b6abc2e
Natalia Kapuscinska <nataliakapuscinska@interia.eu>
2025-12-02 17:14:41 +0100

    without dvt problems with random

--- lab06/run/INCA_libs/run.lnx8664.24.09.d/files.ts
+++ lab06/run/INCA_libs/run.lnx8664.24.09.d/files.ts
@@ -1,5 +1,5 @@
 1750968485 /home/student/nkapuscinska/cds.lib
-1764685552 /home/student/nkapuscinska/VDIC/MTM_VDIC_nkapuscinska/lab06/tb/fifomult_tb_pkg.svh
-1764640571 /home/student/nkapuscinska/VDIC/MTM_VDIC_nkapuscinska/lab06/tb/switch_bfm.svp
+1764691592 /home/student/nkapuscinska/VDIC/MTM_VDIC_nkapuscinska/lab06/tb/fifomult_tb_pkg.svh
+1764691713 /home/student/nkapuscinska/VDIC/MTM_VDIC_nkapuscinska/lab06/tb/switch_bfm.svp
 1760455770 /home/student/nkapuscinska/VDIC/MTM_VDIC_nkapuscinska/common/simple_uart_switch.svp
 1763505775 /home/student/nkapuscinska/VDIC/MTM_VDIC_nkapuscinska/lab06/tb/top.svp
