2ad683c9e16d09b92c45b3883b2dbce1a21611b1
Natalia Kapuscinska <65508960+nkapuscinska@users.noreply.github.com>
2025-12-05 22:05:09 +0100

    Delete lab03/run directory

--- lab03/run/INCA_libs/run.lnx8664.24.09.d/files.ts
+++
@@ -1,9 +0,0 @@
-1750968485 /home/student/nkapuscinska/cds.lib
-1762990927 /home/student/nkapuscinska/VDIC/MTM_VDIC_nkapuscinska/lab03/tb/fifomult_tb_pkg.svp
-1762993238 /home/student/nkapuscinska/VDIC/MTM_VDIC_nkapuscinska/lab03/tb/switch_bfm.svp
-1762991041 /home/student/nkapuscinska/VDIC/MTM_VDIC_nkapuscinska/lab03/tb/monitor.svp
-1762993547 /home/student/nkapuscinska/VDIC/MTM_VDIC_nkapuscinska/lab03/tb/scoreboard.svp
-1762990337 /home/student/nkapuscinska/VDIC/MTM_VDIC_nkapuscinska/lab03/tb/coverage.svp
-1762993275 /home/student/nkapuscinska/VDIC/MTM_VDIC_nkapuscinska/lab03/tb/tp_gen.svp
-1760455770 /home/student/nkapuscinska/VDIC/MTM_VDIC_nkapuscinska/common/simple_uart_switch.svp
-1762989858 /home/student/nkapuscinska/VDIC/MTM_VDIC_nkapuscinska/lab03/tb/top.svp
--- lab03/run/INCA_libs/run.lnx8664.24.09.d/worklib_-3.ts
+++
@@ -1,2 +0,0 @@
-1727027473 /eda/cadence/2024-25/RHELx86/XCELIUM_24.09.005/tools.lnx86/methodology/UVM/CDNS-1.1d/sv/src/uvm_pkg.sv
-1727027472 /eda/cadence/2024-25/RHELx86/XCELIUM_24.09.005/tools.lnx86/methodology/UVM/CDNS-1.1d/additions/sv/cdns_uvm_pkg.sv
